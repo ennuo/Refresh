acae2d1c2a7a6d322566fa4b611c979a80d80026
Aidan <ajr.uiz2048@gmail.com>
2024-01-05 23:10:49 -0500

    Forgot to set XML element tags for the rest of the version properties

--- Refresh.GameServer/Types/Levels/GameLevelVersion.cs
+++ Refresh.GameServer/Types/Levels/GameLevelVersion.cs
@@ -13,8 +13,8 @@ public partial class GameLevelVersion : IRealmObject, ISequentialId
     [PrimaryKey] [XmlElement("id")] public int SequentialId { get; set; }
     [XmlIgnore] public GameLevel Level { get; set; } = null!;
     [XmlElement("timestamp")] public long Timestamp { get; set; }
-    public string LevelAsset { get; set; }
-    public string Title { get; set; }
-    public string Icon { get; set; }
-    public string Description { get; set; }
+    [XmlElement("level")] public string LevelAsset { get; set; }
+    [XmlElement("title")] public string Title { get; set; }
+    [XmlElement("icon")] public string Icon { get; set; }
+    [XmlElement("description")] public string Description { get; set; }
 }
